693dac0108484e1ff1f5840e7bd58a4c003a3a91
Masood <masoodacheampong@gmail.com>
2026-07-28 13:50:26 +0000

    feat: enhance GraphQL schema and resolvers for listing details

--- src/datasources/listing-api.ts
+++ src/datasources/listing-api.ts
@@ -4,7 +4,10 @@ import { Listing } from "../types";
 export class ListingAPI extends RESTDataSource {
   baseURL = "https://rt-airlock-services-listing.herokuapp.com/";
 
-  getFeaturedListings() {
+  getFeaturedListings(): Promise<Listing[]> {
     return this.get<Listing[]>("featured-listings");
+  }
+  getListing(listingId: string): Promise<Listing> {
+    return this.get<Listing>(`listings/${listingId}`);
   }
 }
